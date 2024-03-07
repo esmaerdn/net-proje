f7fe52519229f3b5b5f651c2bebc8fb229922e30
Esma Nur ERDEN <162516419+esmaerdn@users.noreply.github.com>
2024-03-07 03:34:56 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Bir sayının mükemmel sayı olup olmadığı kontrol eden algoritma yazınız.
+    
+
+    Console.Write(" Lütfen bir sayı giriniz: ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    int toplam = 0;
+
+    for (int i= 1; i < sayi; i++) 
+    {
+        if (sayi % i == 0)
+        {
+            toplam += i;
+        }
+
+    }
+   
+    if (toplam == sayi)
+    {
+        Console.WriteLine("'" + sayi + "'" + "sayısı Mükemmel Sayıdır.");
+    }
+    
+    else
+    {
+        Console.WriteLine("'" + sayi + "'" + "sayısı Mükemmel Sayı değildir.");
+    }
+        Console.ReadLine();
