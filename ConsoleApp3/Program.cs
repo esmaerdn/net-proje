f30b7799e6dd6e94e210f3434bc0098c77c2314b
Esma Nur ERDEN <162516419+esmaerdn@users.noreply.github.com>
2024-03-07 03:04:38 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// While döngüsünde parantez içindeki şart sağlanıyorsa döngü devam eder, sağlanmaz ise döngüden çıkar.
+
+        int sayi = 0;
+        int toplam = 0;
+
+            while (sayi <= 50)
+            {
+                if (sayi % 2 == 0)
+                    toplam += sayi;
+                sayi++;
+            }
+
+            Console.WriteLine("50'ye kadar olan çift sayılar toplamı:" + toplam);
+            Console.ReadKey();
+            Console.WriteLine("--------------------------------------------------");
+
+
+// do-while döngüsü de tıpkı while döngüsü gibidir, while döngüsünden farklı olarak döngü içindeki kısım en az bir defa işletilir.
+
+        char i = 'A';
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            while (i <= 'Z');
+            Console.ReadLine();
