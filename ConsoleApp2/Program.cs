3636125407af1044dd7ab6a6096a2b8ab7f748f0
Esma Nur ERDEN <162516419+esmaerdn@users.noreply.github.com>
2024-03-07 03:03:55 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Kullanıcıdan kaç ürün almak istediğini soran,her ürünün fiyatını alarak toplam alışveriş tutarını hesaplayan bir algoritma yazınız.
+
+Console.Write("Almak istediğiniz ürün miktarını yazınız:");
+    int totalProduct = Convert.ToInt32(Console.ReadLine());
+    double totalPrice = 0;
+
+    for (int i = 1; i <= totalProduct; i++)
+    {
+        Console.Write("Ürün fiyatını giriniz:");
+        double productPrice = Convert.ToDouble(Console.ReadLine());
+        totalPrice += productPrice;
+    }
+
+    Console.WriteLine("Toplam alışveriş tutarınız:" + totalPrice + " " + "TL");
